bad9eecd18c142651c3ae446569f12a1145342d1
Gauri <gauridhumal@gmail.com>
2020-04-18 22:08:20 +0100

    Version 4 - Set 4 tests to fail

--- src/test/java/Tests1.java
+++ src/test/java/Tests1.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.*;
 import org.junit.runners.MethodSorters;
@@ -8,6 +9,7 @@ public class Tests1 {
     @Test
     public void test1(){
         System.out.println("executing test1");
+        Assert.fail();
     }
     @Test
     public void test2(){
@@ -21,5 +23,22 @@ public class Tests1 {
     public void test4(){
         System.out.println("executing test4");
     }
-
+    @Test
+    public void test5(){
+        System.out.println("executing test5");
+    }
+    @Test
+    public void test6(){
+        System.out.println("executing test6");
+        Assert.fail();
+    }
+    @Test
+    public void test7(){
+        System.out.println("executing test7");
+    }
+    @Test
+    public void test8(){
+        System.out.println("executing test8");
+        Assert.fail();
+    }
 }
